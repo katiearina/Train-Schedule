5f8a1728a6045ccc048849726cb8c20a2c51c54f
Katie <katiearina@gmail.com>
2017-04-02 15:36:29 -0400

    :tram::sweat: HTML mostly laid out. Need to get Firebase and JS added and then finesse the design a bit more. Stinkin' tables.

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------------
+// VARIABLE DECLARATIONS!
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAb6Aa0FeF6yAqjGqVqjGmGJ-kzl8SmsKU",
+	authDomain: "train-schedule-6c731.firebaseapp.com",
+	databaseURL: "https://train-schedule-6c731.firebaseio.com",
+	projectId: "train-schedule-6c731",
+	storageBucket: "train-schedule-6c731.appspot.com",
+	messagingSenderId: "663388181090"
+};
+
+firebase.initializeApp(config);
+
+
+
+//---------------------------------------------------------------------------
+// FUNCTION DECLARATIONS!
+
+
+
+//---------------------------------------------------------------------------
+// ACTUAL FUNCTIONAL BITS!
